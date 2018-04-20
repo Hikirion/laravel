1944b8fbc797479b4b1e76c621fc7a5f0456b2c8
JackKonamiLondon <acerliquide1my@gmail.com>
2018-04-21 00:32:33 +0300

    Base frontend structure

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts/_layout')
+@section("content")
+<h1>content</h1>
+@stop
---
+++ resources/views/layouts/layouts/scripts.blade.php
@@ -0,0 +1,4 @@
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/jquery.js') }}"></script>
+<script src="{{ asset('js/typeahead.min.js') }}"></script>
